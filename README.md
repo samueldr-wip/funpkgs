f8d3bc03557803ae093d32eef5bd864bc48f9723
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-11-03 23:04:32 -0500

    Add shell.nix and accompanying README note.

--- README.md
+++ README.md
@@ -6,3 +6,18 @@ stuff out. Don't expect anything here.*
 
 * * *
 
+You probably want to `nix-shell` before using `nix-build` or `nix build`, to
+ensure no default sandbox impurities.
+
+Sandbox impurities??
+--------------------
+
+We are wrapping nix and nix-build in `shell.nix`.
+
+The goal of that minimal wrapper is to completely opt-out any and all sandbox
+paths that may exist with default Nix configurations.
+
+Building with the default sandbox settings *could* result in different results.
+
+`Funpkgs` does not aim to be POSIX compliant with regards to `/bin/sh` during
+builds.
---
+++ shell.nix
@@ -0,0 +1,25 @@
+let
+  nixpkgs = import (fetchNixpkgs {
+    rev = "1dc37370c489b610f8b91d7fdd40633163ffbafd";
+    sha256 = "1qvfxf83rya7shffvmy364p79isxmzcq4dxa0lkm5b3ybicnd8f4";
+  }) {};
+
+  fetchNixpkgs = {rev, sha256}: fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/.tar.gz";
+    inherit sha256;
+  };
+
+  nixWrapper = nixpkgs.writeShellScriptBin "nix" ''
+    exec env -i ${nixpkgs.nix}/bin/nix --option sandbox-paths "" "$@"
+  '';
+
+  nix-buildWrapper = nixpkgs.writeShellScriptBin "nix-build" ''
+    exec env -i ${nixpkgs.nix}/bin/nix-build --option sandbox-paths "" "$@"
+  '';
+in
+  nixpkgs.mkShell {
+    buildInputs = [
+      nix-buildWrapper
+      nixWrapper
+    ];
+  }
