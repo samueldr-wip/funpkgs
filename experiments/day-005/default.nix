d44e5fdbcb5d9a6581ea5d25d8d74872fb056a5c
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-11-05 15:05:21 -0500

    day-005: Build heirloom-sh using boot helpers

--- experiments/day-005/default.nix
+++ experiments/day-005/default.nix
@@ -45,8 +45,42 @@ let
     }
   '';
 
-  # TODO: re-build here using _boot helpers.
-  heirloom-sh = _boot.heirloom-sh;
+  heirloom-sh = runExecline "heirloom-sh" {
+    src = externalSrc.heirloom;
+    CC = "${tinycc}/bin/tcc";
+    PATH="${_boot.netbsd-make}/bin";
+  } ''
+    importas out out
+    importas src src
+    importas CC CC
+
+    ${header "Source phase"}
+    ${commands [
+      "cp -vr \${src}/heirloom-sh /build/src"
+    ]}
+    execline-cd /build/src
+
+    ${header "Patch phase"}
+    ${commands [
+      "patch -p2 -i ${./heirloom/0001-heirloom-sh-Work-around-tcc-compilation-bug.patch}"
+    ]}
+
+    ${header "Build phase"}
+    ${commands [
+      "make CC=\${CC} sh"
+      "make CC=\${CC} sh.1.out"
+    ]}
+
+    ${header "Install phase"}
+    ${commands [
+      # Not using `make install` as it is way too "old UNIX" style.
+      "mkdir -p \${out}/bin \${out}/share/man/man1"
+      "cp -v sh \${out}/bin/sh"
+      "ln -s \${out}/bin/sh \${out}/bin/rsh"
+      "ln -s \${out}/bin/sh \${out}/bin/jsh"
+      "cp -v sh.1.out \${out}/share/man/man1/sh.1"
+    ]}
+  '';
 
   netbsd = {
     make = runExecline "netbsd-make-9.0" rec {
@@ -107,103 +141,6 @@ let
     '';
   };
 
-  # An heirloom-sh that's being built without external utilities.
-  heirloom-sh = runExecline "heirloom-sh" {
-    src = externalSrc.heirloom.sh;
-    CC = "${tinycc}/bin/tcc";
-  } ''
-    importas out out
-    importas src src
-    importas CC CC
-
-    foreground {
-      printf "\n:: Source phase\n"
-    }
-
-    ${commands [
-      "cp -vr \${src} /build/src"
-    ]}
-
-    execline-cd src
-
-    ${commands [
-      "patch -p2 -i ${./heirloom/0001-heirloom-sh-Work-around-tcc-compilation-bug.patch}"
-    ]}
-
-    foreground {
-      printf "\n:: Build phase\n"
-    }
-
-    ${
-      let
-        CFLAGS = "-D_GNU_SOURCE -D_FILE_OFFSET_BITS=64L";
-        LDFLAGS = "";
-      in
-      commands [
-      "$CC ${CFLAGS} -c args.c"
-      "$CC ${CFLAGS} -c blok.c"
-      "$CC ${CFLAGS} -c bltin.c"
-      "$CC ${CFLAGS} -c cmd.c"
-      "$CC ${CFLAGS} -c ctype.c"
-      "$CC ${CFLAGS} -c defs.c"
-      "$CC ${CFLAGS} -c echo.c"
-      "$CC ${CFLAGS} -c error.c"
-      "$CC ${CFLAGS} -c expand.c"
-      "$CC ${CFLAGS} -c fault.c"
-      "$CC ${CFLAGS} -c func.c"
-      "$CC ${CFLAGS} -c hash.c"
-      "$CC ${CFLAGS} -c hashserv.c"
-      "$CC ${CFLAGS} -c io.c"
-      "$CC ${CFLAGS} -c jobs.c"
-      "$CC ${CFLAGS} -c macro.c"
-      "$CC ${CFLAGS} -c main.c"
-      "$CC ${CFLAGS} -c msg.c"
-      "$CC ${CFLAGS} -c name.c"
-      "$CC ${CFLAGS} -c print.c"
-      "$CC ${CFLAGS} -c pwd.c"
-      "$CC ${CFLAGS} -c service.c"
-      "$CC ${CFLAGS} -c setbrk.c"
-      "$CC ${CFLAGS} -c stak.c"
-      "$CC ${CFLAGS} -c string.c"
-      "$CC ${CFLAGS} -c test.c"
-      "$CC ${CFLAGS} -c ulimit.c"
-      "$CC ${CFLAGS} -c word.c"
-      "$CC ${CFLAGS} -c xec.c"
-      "$CC ${CFLAGS} -c gmatch.c"
-      "$CC ${CFLAGS} -c getopt.c"
-      "$CC ${CFLAGS} -c strsig.c"
-      "$CC ${CFLAGS} -c version.c"
-      "$CC ${CFLAGS} -c mapmalloc.c"
-      "$CC ${CFLAGS} -c umask.c"
-      "$CC ${LDFLAGS} args.o blok.o bltin.o cmd.o ctype.o defs.o echo.o error.o expand.o fault.o func.o hash.o hashserv.o io.o jobs.o macro.o main.o msg.o name.o print.o pwd.o service.o setbrk.o stak.o string.o test.o ulimit.o word.o xec.o gmatch.o getopt.o strsig.o version.o mapmalloc.o umask.o  -o sh"
-    ]}
-
-    foreground {
-      printf "\n:: Check phase\n"
-    }
-
-    ${commands [
-      ''./sh -c "type echo"''
-      ''./sh -c "echo ok"''
-      ''./sh -c "echo ok > out.txt"''
-      ''./sh -c "cat out.txt"''
-      ''./sh -c "echo one two three"''
-
-      # This broke at some point; any directory available.
-      # See patch.
-      ''./sh -c "echo /tmp/"''
-    ]}
-
-    foreground {
-      printf "\n:: Install phase\n"
-    }
-
-    ${commands [
-      "mkdir -p \${out}/bin"
-      "cp -v sh \${out}/bin/sh"
-    ]}
-
-  '';
 
   test-make = runExecline "test-make" {
     src = ./test-make;
